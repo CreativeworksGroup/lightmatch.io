4071f23670c3b63b3b4a9e6d3057d20a942b54ea
Kwun Yeung <kwun@creativeworks.com.hk>
2016-07-02 01:33:04 +0800

    added multilingual support

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -9,3 +9,11 @@ Template.header.helpers({
         }
     }
 });
+
+Template.header.events({
+    'click a.lang': function(e){
+        e.preventDefault();
+        var lang = $(e.target).data("lang");
+        TAPi18n.setLanguage(lang);
+    }
+});
