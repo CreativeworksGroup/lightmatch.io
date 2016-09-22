f2d17d5001b00d532cae991b7998ed0af13ed9f7
Sean Liang <sean@creativeworks.com.hk>
2016-09-22 16:53:48 +0800

    -》add meteor method -》deny update user created date

---
+++ server/methods.js
@@ -0,0 +1,10 @@
+// editProfileMethod
+Meteor.methods({
+	'editProfileMethod' : function(doc) {
+
+		check(doc, Schema.createUserFormSchema);
+		// `doc` will contains the field who are in the `Schema.createUserFormSchema`
+		var newUser = Accounts.createUser(/* standard args */);
+		Meteor.users.update(newUser /* , set the extra information, like status */);
+	}
+});
